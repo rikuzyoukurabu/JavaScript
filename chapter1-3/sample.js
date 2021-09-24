07106a7fee8e926edb13f5955612c4d47f752a16
Yu Urakawa <rikuzyoukurabu@gmail.com>
2021-09-24 16:54:36 +0900

    1-3 Finish

---
+++ chapter1-3/sample.js
@@ -0,0 +1,76 @@
+// // varによる再宣言
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// letによる再宣言
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "yucco"
+// console.log(nickname)
+
+// constによる再宣言
+// const nickname = "yucco"
+// console.log(nickname)
+// const nickname = "haruka"
+// console.log(nickname)
+
+// varによる再代入
+// var nickname = "yucco"
+// console.log(nickname)
+// nickname = "kayoko"
+// console.log(nickname)
+
+// letによる再代入
+// let nickname = "kayoko"
+// console.log(nickname)
+// nickname = "yukachin"
+// console.log(nickname)
+
+// constによる再代入
+// const nickname = "mai"
+// console.log(nickname)
+// nickname = "ryouko"
+// console.log(nickname)
+
+// var str = "webcamp"
+
+// function foo(){
+//   console.log(stc)
+//   var y ="hello"
+// }
+
+// foo()
+// console.log(y)
+
+// function foo(){
+//   let x = "webcamp"
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+
+// foo()
+
+// for (let i = 0; i<10; i++){
+//   console.log(i)
+// }
+
+// console.log(i)
+
+// var str = "webcamp"
+
+// function foo(){
+//   console.log(str)
+//   var str = "dmm webcamp"
+//   console.log(str)
+// }
+
+// foo()
+
+// for (let i = 0; i<10; i++){
+//   console.log(i);
+// }
